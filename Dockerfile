b67ecaec00b86d8b02b1a5aeb1d8a45be4a62c38
Ravi Kumar K <ravikumar.k1@tataelxsi.co.in>
2026-05-26 11:10:53 +0530

    initial spring boot CI/CD project

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM eclipse-temurin:21-jdk-alpine
+
+WORKDIR /app
+
+COPY target/*.jar app.jar
+
+EXPOSE 8989
+
+ENTRYPOINT ["java","-jar","app.jar"]
+
---
+++ src/main/java/in/elxsi/ravi/controller/HelloController.java
@@ -0,0 +1,18 @@
+package in.elxsi.ravi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Spring Boot app deployment using gitHub,Jenkins,Docker and Ec2";
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        return "healthy";
+    }
+}
